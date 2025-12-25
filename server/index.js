218eee7294734c7621244076965aae34d857048f
sarvezz17 <svsarvesh264@gmail.com>
2025-12-25 21:06:00 +0530

    Update static file serving and add index route
    Refactor static file serving and add index route

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
-
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server);
 
-app.use(express.static("client"));
+// ✅ Serve frontend files
+app.use(express.static(path.join(__dirname, "../client")));
+
+// ✅ Serve index.html on root "/"
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
 
 const MAX_USERS = 14;
 const BID_TIME = 15;
